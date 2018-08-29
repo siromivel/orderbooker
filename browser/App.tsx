07c82458afb537228d0c00df6432874c82bb1c9e
Levi Morris <levi.morris@campspot.com>
2018-08-29 08:34:46 -0600

    Add some tooling; get a basic data fetch in the react app

--- browser/App.tsx
+++ browser/App.tsx
@@ -3,13 +3,23 @@ import React, { Component } from "react";
 class App extends Component {
   constructor(props: object) {
     super(props);
-    this.state = {};
+    this.state = { orderbook: {} };
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:1420/api/orderbook/combined')
+      .then(response => response.json())
+      .then(orderbook => this.setState({ orderbook: orderbook }));
   }
 
   render() {
     return (
       <div>
         Send Corn
+        <div>
+          Enjoy this
+          {JSON.stringify(this.state)}
+        </div>
       </div>
     )
   }
